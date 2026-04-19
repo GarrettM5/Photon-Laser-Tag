ab812cc74a65821b5f04358d8615ec2b48abfaab
djones12254 <djones12254@gmail.com>
2026-04-19 12:42:02 -0500

    Update database.py - Sprint 4 changes

--- src/database.py
+++ src/database.py
@@ -21,12 +21,15 @@ class DatabaseManager:
 
     def get_player_name(self, player_id):
         # Looks up players codename basad on their user ID
-        if not self.conn: return None
+        if not self.conn: 
+            return None
         try:
             cursor = self.conn.cursor()
             query = "SELECT codename FROM players WHERE id = %s"
             cursor.execute(query, (player_id,))
             result = cursor.fetchone()
+
+            # return the name if found, otherwise return none
             if result:
                 return result[0] # return the name found
             return None
@@ -39,20 +42,24 @@ class DatabaseManager:
         if not self.conn: return
         try:
             cursor = self.conn.cursor()
+            # update existing player to prevent db crash from duplicate ids
             if self.get_player_name(player_id):
-                # update existing to prevent db crash from duplicate IDs
                 query = "UPDATE players SET codename = %s WHERE id = %s"
                 cursor.execute(query, (codename, player_id))
+            # insert brand new player
             else:
-                # insert new player
                 query = "INSERT INTO players (id, codename) VALUES (%s, %s)"
                 cursor.execute(query, (player_id, codename))
-            self.conn.commit() # saves changes
+                
+            # save changes to the database
+            self.conn.commit() 
             print(f"Saved Player {player_id}: {codename}")
         except Exception as e:
             print(f"Error saving player: {e}")
-            self.conn.rollback() # undos
+            # undo the transaction if an error occurred
+            self.conn.rollback()
 
     def close(self):
+        # safely close the database connectio
         if self.conn:
             self.conn.close()
